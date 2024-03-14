3fd101880464f7afddd9e9a38cbdbd1432a30388
Paulina Udeh <udehpaulina93@gmail.com>
2024-03-14 17:16:18 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """A type-annotated function to_str that takes n as argument
+       and returns the string representation of te float
+    """
+    return str(n)

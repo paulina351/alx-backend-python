4d1648c4382be3eee5a0098e691a0c8df6160b29
Paulina Udeh <udehpaulina93@gmail.com>
2024-04-11 17:39:24 +0000

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """a coroutine called async_generator that takes no arguments.
+    """
+    for _ in range(0, 10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)

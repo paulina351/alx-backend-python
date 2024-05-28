cd1b45829e293c809a733165fee44bd22bdccba8
Paulina Udeh <udehpaulina93@gmail.com>
2024-05-28 05:40:58 +0100

    mock a HTTP call

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -39,9 +39,37 @@ class TestGetJson(unittest.TestCase):
     @patch('utils.get_json')
     def test_get_json(self, test_url, test_payload):
         """A method that works well with get_json."""
+        # configure the value returned by calling the mock with Mock class:
         mock_cls = Mock()
         mock_cls.json.return_value = test_payload
         with patch('request.get', return_value=mock_cls):
             res = get_json(test_url)
+            # Test that the mocked get method was called exactly once
+            # (per input) with test_urls as argument.
             mock_cls.json.assert_called_once()
+            # Test that the output of get_json is equak to test_payload
             self.assertEqual(res, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Parameterize and patch."""
+    def test_memorize(self):
+        """A method to test the function memorize."""
+        class TestClass:
+            """A class underneath a class."""
+
+            def a_method(self):
+                """A method underneath a class."""
+                return 42
+
+            @memorize  # type: ignore
+            def a_property(self):
+                """A Second method underneath a class."""
+                return self.a_method()
+
+        with patch.object(TestClass,
+                          'a_method', return_value=42) as mock_method:
+            product_cls = TestClass()
+            product_cls.a_property
+            product_cls.a_property
+            mock_method.assert_called_once()
